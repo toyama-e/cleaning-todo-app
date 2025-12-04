833370a509e8dd4635a38e3f40033000f8498d62
toyama-e <erip0719@gmail.com>
2025-12-05 05:29:13 +0900

    feat: tasks_controller.rb に today アクションを追加、routes.rb にルートを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-
-  # localhost:4000/api/tasks
   namespace :api do
+    # /api/tasks/today
+    get "tasks/today", to: "tasks#today"
+
     resources :tasks
     resources :users, only: [:index]
     resources :cleaning_areas, only: [:index]
